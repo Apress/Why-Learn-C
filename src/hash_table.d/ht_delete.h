f736a38ef666818df8aef5bca6ae3f9cc26b1c30
Paul J. Lucas <paul@lucasmail.org>
2025-04-06 05:55:45 -0700

    Added != nullptr

--- src/hash_table.d/ht_delete.h
+++ src/hash_table.d/ht_delete.h
@@ -1,7 +1,7 @@
 void ht_delete( struct hash_table *ht,
                 struct ht_entry *entry ) {
   entry->prev->next = entry->next;
-  if ( entry->next )
+  if ( entry->next != nullptr )
     entry->next->prev = entry->prev;
   free( entry );
   --ht->size;
